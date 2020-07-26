e6b60d67543597493cd26933a08b9a495112f49d
isrivastav99 <ishaan@gmail.com>
2020-07-26 23:02:55 +0530

    keylogger begin

---
+++ advklogger/Helper.h
@@ -0,0 +1,14 @@
+#ifndef HELPER_H
+#define HELPER_H
+#include <ctime>
+#include <string>
+#include <sstream>
+
+namespace Helper{
+    template <typename T>
+
+
+    std::string ToString(const T&)
+}
+
+#endif // HELPER_H
